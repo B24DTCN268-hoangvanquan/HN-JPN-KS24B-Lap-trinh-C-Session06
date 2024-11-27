ff5a53122f606bc5ca03de2b2ff2fc4f71cc0fa0
B24DTCN268-hoangvanquan <hoangvanquan1154@gmail.com>
2024-11-27 11:27:46 +0700

    bài tập ss6-3

---
+++ Session06-3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int pass =05042006;
+	int mk;
+	printf("nhap mat khau\n");
+	scanf("%d",&mk);
+	if (mk=pass) {
+		printf("Mat khau dúng!\n");
+	} else {
+        printf("Mât khau sai!\n");
+	}
+	return 0;
+}
